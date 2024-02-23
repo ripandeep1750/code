cfd54cc01fa33a052cbcb70b7449ba25b4cf45bc
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2024-02-23 17:50:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+''' I attempted a popularity classification of the shares, but the model accuracy wasn't that awesome. In this notebook, I will attempt to 
+improve that previous model accuracy, and also attempt to do some other classfication challenges.
+There will emphasize on feature selection algorthims that can be used in modelling the system for better results. Also, aside using Accuracy
+as the evulation metric, I will also consider some other metric evaluation.
+The process followed is highlighted below:
+Data Cleaning - Noise removal
+Data Transformation - Transform using log-transformation
+Data Clustering - Grouping Similar Articles together.
+Feature Selection and Evaluation
+Machine Learning Classification
+Summary and Conclusion.  '''
 
